9e24be5628f186532d3d918847f027eea3f4c974
Rafa Ruiz <rruizher@redhat.com>
2022-03-23 12:36:32 +0100

    Doing things using the API

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,4 @@
+#In IdM machine:
 echo 'redhat' | ipa user-add cloudadmin --first=Superadmin --last=Superadmin --password
 
 echo 'redhat' | ipa user-add alice --first=Alice --last=Deployments --password
@@ -7,8 +8,17 @@ ipa group-add deployers
 ipa group-add-member deployers --users=alice
 ipa group-add-member developers --users=bob
 
+
+#In workstation machine:
+##Admin user from IdM: cloudamin. Use in the GE. Is it needed also in ocp-mng? Probably yes
+oc login -u admin -p redhat https://api.ocp4-mng.example.com:6443
 oc adm policy add-cluster-role-to-user admin cloudadmin
-T%hen, 
+oc login -u admin -p redhat https://api.ocp4.example.com:6443
+oc adm policy add-cluster-role-to-user admin cloudadmin
+
+
+#Then, 
+oc new-project quay-enterprise
 oc create secret generic --from-file config.yaml=./config.yaml --from-file ldap.crt=./ca.crt init-config-bundle-secret
 oc apply -f quay-registry.yaml
 podman login -u alice company-registry-quay-quay-enterprise.apps.ocp4.example.com
@@ -18,6 +28,41 @@ podman push hello-world-nginx:latest company-registry-quay-quay-enterprise.apps.
 
 
 ##*************************
-#Create 'finance' org.
+#Create 'acme-inc' org.
+#Create 'finance' org from API:
+
+
+curl -X POST -H 'Authorization: Bearer Ek4EZY5xbFxNGniKALHkBS0mctPzRwKhCxYqXDDt' -H 'Content-Type: application/json' -d '{"name":"finance","email":"cloudadmin@example.com"}'  https://company-registry-quay-quay-enterprise.apps.ocp4.example.com/api/v1/organization/
+
+
 #Create 'development' repository
+curl -X POST https://company-registry-quay-quay-enterprise.apps.ocp4.example.com/api/v1/repository -H 'Authorization: Bearer Ek4EZY5xbFxNGniKALHkBS0mctPzRwKhCxYqXDDt' -H "Content-Type: application/json" -d '
+{"namespace":"finance","repository":"development","description":"Repo for images from the development team","visibility":"private"}' | jq
+
 #Create 'production' repository
+curl -X POST https://company-registry-quay-quay-enterprise.apps.ocp4.example.com/api/v1/repository -H 'Authorization: Bearer Ek4EZY5xbFxNGniKALHkBS0mctPzRwKhCxYqXDDt' -H "Content-Type: application/json" -d '
+{"namespace":"finance","repository":"production","description":"Repo for images to use in production","visibility":"private"}' | jq
+
+#Create 'deployers' team
+
+
+curl -X PUT -H "Authorization: Bearer Ek4EZY5xbFxNGniKALHkBS0mctPzRwKhCxYqXDDt" https://https://company-registry-quay-quay-enterprise.apps.ocp4.example.com/api/v1/organization/finance/team/deployers -H "Content-Type: application/json" --data '{"name": "deployers", "role": "member", "description": "People that deploy"}' | jq
+
+
+#Create 'developers' team
+curl -X PUT -H "Authorization: Bearer Ek4EZY5xbFxNGniKALHkBS0mctPzRwKhCxYqXDDt" https://https://company-registry-quay-quay-enterprise.apps.ocp4.example.com/api/v1/organization/finance/team/developers -H "Content-Type: application/json" --data '{"name": "developers", "role": "member", "description": "People that develop"}' | jq
+
+
+
+#Synchonize teams with IdM
+#sync ldap:
+curl -X POST -H "Authorization: Bearer Ek4EZY5xbFxNGniKALHkBS0mctPzRwKhCxYqXDDt" \
+       -H "Content-type: application/json" \
+       -d '{"group_dn": "cn=developers,cn=groups"}' \
+       https://company-registry-quay-quay-enterprise.apps.ocp4.example.com/api/v1/organization/finance/team/developers/syncing
+
+
+curl -X POST -H "Authorization: Bearer Ek4EZY5xbFxNGniKALHkBS0mctPzRwKhCxYqXDDt" \
+       -H "Content-type: application/json" \
+       -d '{"group_dn": "cn=deployers,cn=groups"}' \
+       https://company-registry-quay-quay-enterprise.apps.ocp4.example.com/api/v1/organization/finance/team/deployers/syncing
