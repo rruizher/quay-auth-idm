1c37391173eda85c672afbafb7c8faf61848250c
Rafa Ruiz <rruizher@redhat.com>
2022-03-23 13:41:24 +0100

    More actions with the API

--- prepare.sh
+++ prepare.sh
@@ -11,6 +11,7 @@ ipa group-add-member developers --users=bob
 
 #In workstation machine:
 ##Admin user from IdM: cloudamin. Use in the GE. Is it needed also in ocp-mng? Probably yes
+##Note: there is a policy in the community collection to do this in all managed clusters
 oc login -u admin -p redhat https://api.ocp4-mng.example.com:6443
 oc adm policy add-cluster-role-to-user admin cloudadmin
 oc login -u admin -p redhat https://api.ocp4.example.com:6443
@@ -28,11 +29,11 @@ podman push hello-world-nginx:latest company-registry-quay-quay-enterprise.apps.
 
 
 ##*************************
-#Create 'acme-inc' org.
+#Create 'quayadmin' org and get token from UI
 #Create 'finance' org from API:
 
 
-curl -X POST -H 'Authorization: Bearer Ek4EZY5xbFxNGniKALHkBS0mctPzRwKhCxYqXDDt' -H 'Content-Type: application/json' -d '{"name":"finance","email":"cloudadmin@example.com"}'  https://company-registry-quay-quay-enterprise.apps.ocp4.example.com/api/v1/organization/
+curl -X POST -H 'Authorization: Bearer Ek4EZY5xbFxNGniKALHkBS0mctPzRwKhCxYqXDDt' -H 'Content-Type: application/json' -d '{"name":"finance","email":"finance@example.com"}'  https://company-registry-quay-quay-enterprise.apps.ocp4.example.com/api/v1/organization/
 
 
 #Create 'development' repository
@@ -66,3 +67,17 @@ curl -X POST -H "Authorization: Bearer Ek4EZY5xbFxNGniKALHkBS0mctPzRwKhCxYqXDDt"
        -H "Content-type: application/json" \
        -d '{"group_dn": "cn=deployers,cn=groups"}' \
        https://company-registry-quay-quay-enterprise.apps.ocp4.example.com/api/v1/organization/finance/team/deployers/syncing
+
+
+
+##Add to the repository repo within the organization orga the team orgateam with role read
+curl -X PUT -H "Authorization: Bearer ${bearer_token}" https://${quay_registry}/api/v1/repository/orga/repo/permissions/team/orgateam -H "Content-Type: application/json" --data '{"role": "read"}' | jq
+
+
+
+
+###Create the robot robby within the organization orga
+curl -X PUT -H "Authorization: Bearer ${bearer_token}" https://${quay_registry}/api/v1/organization/orga/robots/robby | jq
+
+#Set the permission write to robot robby for repository repo within organization orga
+curl -X PUT -H "Authorization: Bearer ${bearer_token}" https://${quay_registry}/api/v1/repository/orga/repo/permissions/user/orga+robby -H "Content-Type: application/json" --data '{"role": "write"}' | jq
